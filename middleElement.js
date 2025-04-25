acd07a0fe3d81194332b79e170ed63880f33a00b
Rashmi92-ha <rashmiks727@gmail.com>
2025-04-25 18:43:10 +0530

    doc: found the middle element of the array #15

---
+++ middleElement.js
@@ -0,0 +1,5 @@
+const array = [1, 4, 3, 5, 7, 897, 434, 6566, 7583];
+
+const middleElement = Math.round((array.length - 1) / 2);
+const middleArray = array[middleElement];
+console.log(middleArray);
